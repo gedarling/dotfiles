4bec978316c98c0fad8c0c39631684065380c4fa
Geoffrey Darling <geoff@mitsi.com>
2018-11-15 19:10:59 -0600

    Update README and crib the bootstrap script from wes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Setup details
 
-tbd
++ Managed with [homeshick](https://github.com/andsens/homeshick)
++ [oh-my-zsh](https://ohmyz.sh/)
++ use bootstrap.sh to get started
---
+++ bootstrap.sh
@@ -0,0 +1,58 @@
+#! /bin/dash
+
+set -e # exit on errors
+
+# htop is frivolous; if it's installed then we've run before
+if ! dpkg -l | grep -q htop; then
+   sudo apt-get update
+   sudo apt-get upgrade
+   sudo apt-get --no-install-recommends install \
+      vim git zsh tmux \
+      curl mtr tree htop
+fi
+
+# generate keys
+if [ ! -e $HOME/.ssh/id_rsa ]; then
+   ssh-keygen -t rsa -b 2048
+   cat ~/.ssh/id_rsa.pub
+   echo "\n\nGive me a GitHub 2FA token to add this to the keylist"
+   echo "You'll need your GitHub password as well"
+   echo "[blank] to add it yourself...\n\n"
+   read otp
+   if [ $otp ]; then
+      curl -i -u gedarling \
+         -H "X-GitHub-OTP: $otp" \
+         -H "Content-Type: application/json" \
+         --data "{\"title\":\"`cat /etc/hostname`-autogen\",\"key\":\"`cat ~/.ssh/id_rsa.pub`\"}" \
+         https://api.github.com/user/keys
+   else
+      echo Press a key to continue
+      read foo
+   fi
+fi
+
+# plenv
+if [ ! -d $HOME/.plenv ]; then
+   git clone https://github.com/tokuhirom/plenv.git ~/.plenv
+   git clone https://github.com/tokuhirom/Perl-Build.git ~/.plenv/plugins/perl-build/
+fi
+
+# homeshick
+if [ ! -d $HOME/.homesick ]; then
+   git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+fi
+
+# ohmyzsh
+# sh -c "$(curl --fail --silent --show-error --location https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+[ ! -d $HOME/.oh-my-zsh ] && \
+   git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+
+HS=$HOME/.homesick/repos/homeshick/bin/homeshick
+[ ! -d $HOME/.homesick/repos/dotfiles ] && \
+   $HS clone --batch git@github.com:wesQ3/dotfiles.git
+$HS link --force
+
+cd $HOME
+
+echo Setting local shell
+chsh --shell /bin/zsh
